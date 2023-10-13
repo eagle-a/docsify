d4a155e47793b5fcec1249477b4ba9da3fd8dc60
eagle-a <2121612901@qq.com>
2023-10-13 20:27:45 +0800

    bat鎵瑰鐞嗚嚜鍔ㄦ帹閫?2023/10/13,20:27:45

---
+++ auto.bat
@@ -0,0 +1,115 @@
+@echo off  
+
+chcp 65001  
+  
+title GIT一键提交  
+color 0D  
+echo 当前目录是：%cd%  
+echo;  
+echo 请先执行f. docsify serve docs
+
+echo; 
+echo f. 快速提交  
+echo 1. 添加变更:git add .  
+echo 2. 提交变更  
+echo 3. //推送到远程自己分支//
+echo 4. 切换到主分支  
+echo 5. 拉取远程主分支  
+echo 6. 主分支合并分支  
+echo 7. 推送到远程主分支  
+echo 8. 切换到分支  
+echo 9. 拉取远程主分支到本地分支  
+echo X. 退出  
+  
+set /p choice=请输入数字选择操作:  
+  
+if "%choice%"=="f" (  
+    echo 请输入提交信息:  
+    set /p declation=  
+    echo 开始添加变更:git add .  
+    git add .  
+    git commit -m "%declation%"  
+    git push -u origin main
+    echo;  
+    goto :continue  
+)  
+
+if "%choice%"=="1" (
+    echo 开始添加变更:git add .
+    git add .
+    echo;
+    goto :continue
+)
+
+if "%choice%"=="2" (
+    set /p declation=输入提交的commit信息:
+    echo;
+    echo 提交变更:git commit -m "%declation%"
+    git commit -m "%declation%"
+    echo;
+    goto :continue
+)
+
+::if "%choice%"=="3" (
+  ::  echo 将变更情况推送到远程自己分支:git push origin hlt
+  ::  git push -u origin main
+   :: echo;
+  ::  goto :continue
+::)
+
+if "%choice%"=="4" (
+    echo 切换到主分支:git checkout main
+    git checkout main
+    echo;
+    goto :continue
+)
+
+if "%choice%"=="5" (
+    echo 本地主分支拉取远程主分支:git pull origin main
+    git pull origin main
+    echo;
+    goto :continue
+)
+
+if "%choice%"=="6" (
+    
+    set /p remoteBranch=请输入远程分支:
+    echo 主分支合并分支:git merge %remoteBranch%
+    git merge %remoteBranch%
+    echo;
+    goto :continue
+)
+
+if "%choice%"=="7" (
+    echo 将变更情况推送到远程主分支:git push origin main
+    git push origin main
+    echo;
+    goto :continue
+)
+
+if "%choice%"=="8" (
+    set /p remoteBranch=请输入远程分支:
+    echo 切换到分支;git checkout %remoteBranch%
+    git checkout hlt
+    echo;
+    goto :continue
+)
+
+if "%choice%"=="9" (  
+    echo 请输入要拉取的远程分支和本地分支名称:  
+    set /p remoteBranch=请输入远程分支:   
+    set /p localBranch=请输入本地分支:   
+    echo 本地分支拉取远程主分支:%localBranch%:git pull origin %remoteBranch%  
+    git pull origin %remoteBranch%  
+    goto :continue  
+)  
+if "%choice%"=="X" (
+    echo 退出!
+    goto :eof
+)
+
+:continue
+echo 执行完毕！
+echo;
+
+pause
