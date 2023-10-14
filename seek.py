5905457de49953e2572a8bc4ce99f4e26eb647f0
eagle-a <2121612901@qq.com>
2023-10-14 21:29:13 +0800

    bat批处理自动推送:周六 2023/10,21:29:13

--- seek.py
+++ seek.py
@@ -1,12 +1,11 @@
 import os
 
-
 def generate_menu(directory):
     menu = []
     dirs = os.listdir(directory)
     for dir in dirs:
         menu.append([dir])
-        menu.append(['\n'+'- [{}]'.format(dir)])
+        menu.append('- [{}]'.format(dir))
         for root, files in os.walk(dir):
             for file in files:
 
@@ -15,10 +14,11 @@ def generate_menu(directory):
                     # 使用 os.path.relpath 来正确地计算相对路径
                     relative_path = os.path.relpath(file_path, directory)
                     # 仅保存文件名和相对路径
-                    menu.appendstr('\n'.join(['- [{}]({})'.format(os.path.basename(file), relative_path)]))
+                    menu.appendstr(
+                        ''.join('- [{}]({})'.format(os.path.basename(file), relative_path)))
                    # menu.append([os.path.basename(file), relative_path])
-   # str(menu)              
     return menu
+
 
 # 注意使用原始字符串来处理路径中的反斜杠
 directory = r'C:\Users\21216\Desktop\math_and_Program\docsify\docsify\docs\book'
@@ -26,7 +26,8 @@ directory = r'C:\Users\21216\Desktop\math_and_Program\docsify\docsify\docs\book'
 if __name__ == "__main__":
     menu = generate_menu(directory)
     location = r'C:\Users\21216\Desktop\math_and_Program\docsify\docsify\docs\book\_sidebar.md'
-    f = open(location, 'w',encoding='utf-8')  
-    for url in menu:  
-         f.write(str(url))  # 添加 '\n' 是为了在每个url后面都有一个新的一行  
+    f = open(location, 'w', encoding='utf-8')
+    for url in menu:
+        f.write(str(url))  # 添加 '\n' 是为了在每个url后面都有一个新的一行
+        f.write('\n')
     f.close()  # 不要忘记关闭文件
