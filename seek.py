1ac7286201f2e0b22c65b17f3f836f4050f42592
eagle-a <2121612901@qq.com>
2023-10-14 22:53:22 +0800

    bat批处理自动推送:周六 2023/10,22:53:22

--- seek.py
+++ seek.py
@@ -3,29 +3,26 @@ import os
 
 def generate_menu(directory):
     menu = []
-
-    for dirs in os.walk(directory):
-        for dirs_1 in dirs:
-            menu.append('['+dirs_1+']')
-            for dirs_2 in os.walk(dirs_1):
-                for files in os.walk(dirs_2):
-                    for root, file in files:
-                        if file.endswith('.md'):
-                            file_path = str(os.path.join(root, file))
-                            relative_path = str(
-                                os.path.relpath(file_path, directory))
-                            menu.appendstr('['+dirs_2+ ']' + '('+relative_path + ')')
-
+    for root, files in os.walk(directory):
+        for file in files:
+            if file.endswith('.md'):
+                file_path = os.path.join(root, file)
+                relative_path = os.path.relpath(file_path, directory)
+            #   relative_path_with_slash = os.path.join('/', relative_path)
+                menu.append( '[' + '/'.join(file) + '](/book/' + relative_path + ')')
     return menu
 
-
-directory = r'C:\Users\21216\Desktop\math_and_Program\docsify\docsify\docs\book'
+directory = r'./docsify/docs/book'
 
 if __name__ == "__main__":
     menu = generate_menu(directory)
     location = r'C:\Users\21216\Desktop\math_and_Program\docsify\docsify\docs\_sidebar.md'
     f = open(location, 'w', encoding='utf-8')
-    for url in menu:
-        f.write(str(url)) 
+    for menu1 in menu:
+        f.write(menu1)
         f.write('\n')
-    f.close() 
+    f.close()
+    # 打开并读取Markdown文件  
+with open(location, 'r', encoding='utf-8') as file:  
+    content = file.read()   
+    file.write(content)
